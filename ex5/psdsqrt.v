b60ff22ba82afc6c7b2d894d1349c179877137c9
Bia Garrido <36662037+bgarrido7@users.noreply.github.com>
2019-11-17 18:21:46 +0000

    Add files via upload

---
+++ ex5/psdsqrt.v
@@ -0,0 +1,125 @@
+/******************************************************************************************
+ **************************** LABORATORY PROJECT 1 - psdsqrt.v ****************************
+ ******************************************************************************************
+ * This project consists in implementing a sequential calculator of the square root for
+ * 32-bit unsigned integer numbers. The circuit is builted as a synthesizable Verilog
+ * module and represents the logic diagram shown in the lab guidelines.
+ * The module descrived in this file is based on the following algorithm (processed in 
+ * bit domain):
+ *   Initial values:
+ *     - xin is the argument of square root
+ *     - testbit = (1<<15) -> initializes the test bit applied in each iteration
+ *     - tempsqrt = 0; -> we start the algorithm from the MSB to the LSB
+ *
+ *   1. testsqrt = testbit | tempsqrt
+ *   2. Comparation between xin and testsqrt*testsqrt
+ *       - If xin >= testsqrt*testsqrt, the value in testsqrt in the present iteration is
+ *           valid until the present bit (MSB, MSB-1, ..., LSB) and tempsqrt = testsqrt;
+ *       - Else, the tempsqrt holds is value.
+ *   3. testbit = testbit >> 1 -> the algorithm advances is executing for the next bit.
+ ******************************************************************************************
+ * Authors:
+ *   - Beatriz Neves Garrido (up201504710@fe.up.pt | bianevesgarrido@gmail.com)
+ *   - Ricardo Barbosa Sousa (up201503004@fe.up.pt | sousa.ricardobarb@gmail.com)
+ ******************************************************************************************/
+
+// Module declaration
+module psdsqrt(
+		input clock,               // Master clock implemented in the module
+		input reset,               // Synchronized reset activeted in the high level
+		input start,               // Signal to start a new square root calculation
+		input stop,                // Load output register
+		input [31:0] xin,          // Operand (unsigned)
+		output reg [15:0] sqrt     // Square root of the operand xin
+    );
+
+`define GROUPID   72'h201504710_201503004
+initial $display("Beatriz Neves Garrido; Ricardo Barbosa Sousa");
+
+// Regs and wires declaration
+reg  [31:0] xin_stored;       // Register that stores the input value to square root's argument
+reg  [15:0] testbit;          // Testbit used in the algorithm application
+reg  [15:0] testbit_mux;      // Result of testbit>>1, except in the start of the algorithm (testbit_mux = 1'b1000_..._0000)
+reg  [15:0] tempsqrt;         // Holds the temporary value of sqrt(xin) -> is also the output originated by the application of the sqrt algorithm
+reg  [15:0] tempsqrt_mux1;    // Selection beetween testsqrt and tempsqrt (this is based on the algorithm)
+reg  [15:0] tempsqrt_mux2;    // Critical for the start of the algorithm: initializes the tempsqrt register in 16h'0000
+wire [31:0] sqtestsqrt;       // Represents the provisional result with power 2 to be compareted with the original value xin
+wire [15:0] testsqrt;         // Test value of each iteration that will be used for comparating testsqrt with xin (xin_stored)
+wire [15:0] testbit_shift;    // Associated with testbit >> 1
+wire comp_xin_sqtestsqrt;     // Indicates the logic result (represented by 1'b1 or 1'b0) of xin_stored >= testsqrt^2 (equals to sqtestsqrt)
+
+// Store the operand that will be used to calculate the square root (register with FF type-D)
+always @( posedge clock )           // Every time that is detected a rising edge relative to clock signal, this process is executed
+begin
+  if ( reset )                      // If reset high then...
+    xin_stored <= 0;                // The reset is applied to the xin_stored register
+  else                              // Else...
+    if ( start )                    // If start...
+      xin_stored <= xin;            // The value in xin is loaded to xin_stored
+end
+
+// Multiplier to generate the test result for the square root calculation of xin
+assign sqtestsqrt = testsqrt * testsqrt;    // The result of testsqrt^2 is wired to sqtestsqrt
+
+// Comparation between the operand xin and (test result)^2
+assign comp_xin_sqtestsqrt = (xin_stored >= sqtestsqrt)? 1'b1 :    // Comparator >= -> TRUE
+                                                         1'b0;     // Comparator >= -> FALSE
+
+// Test bit generator
+assign testbit_shift = (testbit >> 1);    // Discards the LSB of testbit
+
+always @*                                 // COMBINATIONAL BLOCK - Multiplexer - This process is always "executed"
+begin
+  if ( start )                            // If start (-> is the select signal in the multiplexer)...
+    testbit_mux = 16'h8000;               // testbit_mux holds the initial value for the algorithm relative to the testbit
+  else                                    // Else...
+    testbit_mux = testbit_shift;          // The multiplexer let it pass the iterations made on testbit (testbit>>1 at each iteration)
+end
+
+always @( posedge clock )                 // Every time that is detected a rising edge relative to clock signal, this process is executed
+begin
+  if ( reset )                            // If reset high then...
+    testbit <= 0;                         // The reset is applied to the testbit register
+  else                                    // Else...
+    testbit <= testbit_mux;               // The value in testbit_mux is loaded to testbit
+end
+
+// Temporary end result generator (provisional value for the sqrt(xin))
+always @*                             // COMBINATIONAL BLOCK - Multiplexer - This process is always "executed"
+begin
+  if ( comp_xin_sqtestsqrt )          // If comp_xin_sqtestsqrt (-> is the select signal in the multiplexer)...
+    tempsqrt_mux1 = testsqrt;         // It means that the real sqrt value is closer to testsqrt
+  else                                // Else...
+    tempsqrt_mux1 = tempsqrt;         // It means that the real sqrt value is closer to tempsqrt
+end
+
+always @*                             // COMBINATIONAL BLOCK - Multiplexer - This process is always "executed"
+begin
+  if ( start )                        // If start (-> is the select signal in the multiplexer)...
+    tempsqrt_mux2 = 0;                // tempsqrt_mux2 holds the initial value for the square root (due to still being unknown, is 0)
+  else                                // Else...
+    tempsqrt_mux2 = tempsqrt_mux1;    // The multiplexer let it pass the output of mux1
+end
+
+always @( posedge clock )         // Every time that is detected a rising edge relative to clock signal, this process is executed
+begin
+  if ( reset )                    // If reset high then...
+    tempsqrt <= 0;                // The reset is applied to the tempsqrt register
+  else                            // Else...
+    tempsqrt <= tempsqrt_mux2;    // The value in tempsqrt_mux2 is loaded to tempsqrt
+end
+
+// Test of square root
+assign testsqrt = (tempsqrt | testbit);    // Originates the test value to be comparated after some steps in each algorithm iteration
+
+// Output relative to the sqrt(xin)
+always @( posedge clock )    // Every time that is detected a rising edge relative to clock signal, this process is executed
+begin
+  if ( reset )               // If reset high then...
+    sqrt <= 0;               // The reset is applied to the sqrt register
+  else                       // Else...
+    if ( stop )              // If stop...
+      sqrt <= tempsqrt;      // The value in tempsqrt is loaded to sqrt (FINAL RESULT OF SQRT(xin))
+end 
+
+endmodule
---
+++ ex5/psdsqrt_tb_base.v
@@ -0,0 +1,183 @@
+/***********************************************************************************
+ * PSDi 2018/19
+ *
+ * Lab 1 - Design and verification of a sequential square root calculator
+ *
+ *   This Verilog code is property of University of Porto
+ *   Its utilization beyond the scope of the course Digital Systems Design
+ *   (Projeto de Sistemas Digitais) of the Integrated Master in Electrical 
+ *   and Computer Engineering requires explicit authorization from the author.
+ *
+ *   jca@fe.up.pt, Oct 2018
+ ***********************************************************************************
+ * Authors of the module psdsqrt:
+ *   - Beatriz Neves Garrido (up201504710@fe.up.pt | bianevesgarrido@gmail.com)
+ *   - Ricardo Barbosa Sousa (up201503004@fe.up.pt | sousa.ricardobarb@gmail.com)
+ ***********************************************************************************/
+`include "psdsqrt.v"
+
+module psdsqrt_tb;
+ 
+// general parameters 
+parameter CLOCK_PERIOD = 10;              // Clock period in ns
+parameter MAX_SIM_TIME = 100_000_000;     // Set the maximum simulation time (time units=ns)
+
+  
+// Registers for driving the inputs:
+reg  clock, reset;
+reg  start, stop;
+reg  [31:0] x;
+
+// Registers used in the verification program:
+parameter PRINT_EN = 1;   // Enables the print in simulation's mode
+reg [31:0] i;             // Variable used to test the module with sereval possibilities
+reg [15:0] testsqrt;      // Holds a possible result for the sqrt(xin)
+reg [15:0] testbit;       // Testbit to execute the algorithm from MSB to LSB
+reg [15:0] tempsqrt;      // Temporary value of sqrt(xin) - after all itereations, tempsqrt = sqrt(xin)
+reg [15:0] iteration;     // Present iteration of the algorithm
+reg [15:0] sqrt_value;    // Output of the verification task
+
+// Wires to connect to the outputs:
+wire [15:0] sqrt;
+
+
+// Instantiate the module under verification:
+psdsqrt psdsqrt_1
+      ( 
+	    .clock(clock), // master clock, active in the positive edge
+        .reset(reset), // master reset, synchronous and active high
+		
+        .start(start), // set to 1 during one clock cycle to start a sqrt
+        .stop(stop),   // set to 1 during one clock cycle to load the output registers
+		
+        .xin(x),       // the operands
+        .sqrt(sqrt)
+        ); 
+      
+        
+//---------------------------------------------------
+// Setup initial signals
+initial
+begin
+  clock = 1'b0;
+  reset = 1'b0;
+  x = 0;
+  start = 1'b0;
+  stop  = 1'b0;
+end
+
+//---------------------------------------------------
+// generate a 50% duty-cycle clock signal
+initial
+begin  
+  forever
+    # (CLOCK_PERIOD / 2 ) clock = ~clock;
+end
+
+//---------------------------------------------------
+// Apply the initial reset for 2 clock cycles:
+initial
+begin
+  # (CLOCK_PERIOD/3)    // wait a fraction of the clock period to 
+                        // misalign the reset pulse with the clock edges:
+  reset = 1;
+  # (2 * CLOCK_PERIOD ) // apply the reset for 2 clock periods
+  reset = 0;
+end
+
+//---------------------------------------------------
+// Set the maximum simulation time:
+initial
+begin
+  # ( MAX_SIM_TIME )
+  $stop;
+end
+
+/***********************************************************************************/
+/*************************** VERIFICATION PROGRAM(BEGIN) ***************************/
+	
+initial
+begin
+  #( 10*CLOCK_PERIOD );            // Wait 10 clock periods
+  reset=1'b1;                      // Execute the reset in the module
+
+  #( 2*CLOCK_PERIOD );             // Wait 10 clock periods
+  reset=1'b0;
+  
+  #( 10*CLOCK_PERIOD );            // Wait 10 clock periods
+  execsqrt( 0 );                   // Test the square root of zero
+
+  for(i = 0; i < 32; i=i+1) begin
+    execsqrt( (1<<i) );            // Test the square root of xin_test - powers of 2
+  end
+  for(i = 0; i < 32; i=i+1) begin
+    execsqrt( (1<<i) + i );        // Test the square root of xin_test - random values
+  end 
+
+  #( 10*CLOCK_PERIOD );            // Wait 10 clock periods
+  reset=1'b1;                      // Execute the reset in the module
+
+  #( 2*CLOCK_PERIOD );             // Wait 10 clock periods
+  reset=1'b0;
+  
+  #( 10*CLOCK_PERIOD );            // Wait 10 clock periods
+  $stop;                           // Stops the simulation after the tests are executed or the MAX_SIM_TIME is reached
+end
+
+/**************************** VERIFICATION PROGRAM(END) ****************************/
+/***********************************************************************************/
+
+
+//---------------------------------------------------
+// Execute a sqrt by simulate the module's execution:
+task execsqrt;
+input [31:0] xin;
+begin
+  x = xin;                        // Apply operands
+  @(negedge clock);
+  start = 1'b1;                   // Assert start
+  @(negedge clock );
+  start = 1'b0;  
+  repeat (16) @(posedge clock);   // Execute division
+  @(negedge clock);
+  stop = 1'b1;                    // Assert stop
+  @(negedge clock);
+  stop = 1'b0;
+  @(negedge clock);
+  
+  // Print the results:
+  // You may not watt to do this when verifying some millions of operands...
+  // Add a flag to enable/disable this print
+  verifysqrt(xin, sqrt_value);
+  if (PRINT_EN) 
+    $display("SQRT(%d) = %d (expected value = %d)", x, sqrt, sqrt_value);
+
+end  
+endtask
+
+/***********************************************************************************/
+/************************** VERIFICATION SQRT TASK(BEGIN) **************************/
+task verifysqrt;
+input  [31:0] xin_sqrt;
+output [15:0] sqrt_output;
+
+begin
+  tempsqrt=0;       // Initial value to the final result
+  testbit=1<<15;    // Variable used for building the result from the MSB to the last significant bit
+
+  for(iteration=0;iteration<16;iteration=iteration+1) begin    // Number of itereations necessary = number of bits in the input / 2
+    testsqrt = testbit | tempsqrt;
+    if(xin_sqrt>=testsqrt*testsqrt) begin    // testsqrt*testsqrt is the tentative result
+      tempsqrt=testsqrt;                     // If true, the final result (sqrt) is greater or equal to testsqrt
+    end
+    testbit = testbit>>1;                    // Test next bit 
+  end
+  sqrt_output=tempsqrt;
+
+end
+endtask
+
+/*************************** VERIFICATION SQRT TASK(END) ***************************/
+/***********************************************************************************/
+
+endmodule
